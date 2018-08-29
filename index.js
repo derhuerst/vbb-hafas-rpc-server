f7984e036c49654cc55e5b6d855f4727f9b5db98
Jannis R <mail@jannisr.de>
2018-08-29 19:23:02 +0200

    vbb-hafas@5

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const http = require('http')
-const hafas = require('vbb-hafas')
+const createHafas = require('vbb-hafas')
 const exposeHafasClient = require('hafas-client-rpc/server')
 
 const port = process.env.PORT || 3000
 
 const httpServer = http.createServer()
+const hafas = createHafas('vbb-hafas-rpc-server')
 const server = exposeHafasClient(httpServer, hafas)
 
 httpServer.listen(port, (err) => {
